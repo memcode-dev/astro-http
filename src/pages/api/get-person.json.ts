c13c8468bf76ffae10c70ceae1767dd0f6062126
MEM <matiasezequielmorellimem@gmail.com>
2025-12-26 10:40:08 -0300

    First Commit

---
+++ src/pages/api/get-person.json.ts
@@ -0,0 +1,15 @@
+
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = async ({ params, request }) => {
+    const person = {
+        name: "John Doe",
+        age: 30
+    }
+    return new Response(JSON.stringify(person), { 
+        status: 201,
+        headers:{
+            "Content-Type": "application/json"
+        }
+    });
+};
